769a9691ce5bcc924cd13eb8da5cf986d51e926a
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-03-11 18:08:31 +0200

    box flow

--- src/OSC/AST.hs
+++ src/OSC/AST.hs
@@ -10,22 +10,20 @@ data Ident = Ident String
   deriving (Eq, Ord)
 
 data Expr
- = Unit
+ = Void
  | Var Ident
  | LitB Bool
  | LitI Int
  | LitF Double
- | Abs Type Type [Statement] Expr
- | App Expr Expr
+ | Binop String Expr Expr
 
 data SType = TBool | I32 | I64 | F32 | F64
 
 data CType
   = TArray SType [Int]
-  | TStruct (Map Ident Type)
-  | TAbs Type Type
+  | TStream 
 
-data Type = TUnit | CType CType | SType SType
+data Type = TVoid | CType CType | SType SType
 
 data Statement
   = SBinding Ident Type Expr
@@ -42,54 +40,52 @@ data Frame = Frame
 
 type MemLayout = Map Ident Frame
 
-stypeSize TBool = 4
-stypeSize I32 = 4
-stypeSize I64 = 8
-stypeSize F32 = 4
-stypeSize F64 = 8
-
-typeSize :: Type -> Int
-typeSize TUnit = 0
-typeSize (SType t) = stypeSize t
-typeSize (CType (TArray t dim)) = stypeSize t * product dim
-typeSize (CType (TStruct m)) = sum [ typeSize t | t <- M.elems m ]
-typeSize (CType (TAbs _ _)) = 4
-
--- eliminate empty complex bindings, e.g. a: f32[] = b: a and b are interchangeable here (but not when they are simple)
--- complex or captured values (including simple ones) go in linear memory
--- captured values are passed as additional reference arguments to the function
-
-inc :: Int -> ST.State Int Int
-inc x = do
-  prev <- ST.get
-  ST.put prev
-  pure (prev + x)
-
-memLayoutE :: Expr -> ST.State Int (Maybe Frame)
-memLayoutE = undefined
-
-memLayoutS :: [Statement] -> Expr -> ST.State Int Frame
-memLayoutS sts ret = undefined
-  where
-    returnBinding :: Maybe Ident
-    returnBinding = case ret of
-      Var v -> Just v
-      _ -> Nothing
-
-    a :: ST.State Int (Map Ident BindingOffset)
-    a = M.fromList . concat <$> sequence
-      [ case st of
-          SBinding n t _
-            | Just n == returnBinding -> pure [(n, ReturnOffset)]
-            | otherwise -> do
-                offset <- inc (typeSize t)
-                pure [(n, OffsetRelToFrame offset)]
-          SFor _ _ sts' -> do
-            frame <- memLayoutS sts' Unit
-            _ <- inc frame.size
-            pure []
-      | st <- sts
-      ]
-
--- inline :: Expr -> Expr -> ParentStatementsZipper -> ParentStatementsZipper
--- inline (App (Abs TUnit rt ss r) Unit) = undefined
+-- stypeSize :: SType -> Int
+-- stypeSize TBool = 4
+-- stypeSize I32 = 4
+-- stypeSize I64 = 8
+-- stypeSize F32 = 4
+-- stypeSize F64 = 8
+-- 
+-- typeSize :: Type -> Int
+-- typeSize (SType t) = stypeSize t
+-- typeSize (CType (TArray t dim)) = stypeSize t * product dim
+-- 
+-- -- eliminate empty complex bindings, e.g. a: f32[] = b: a and b are interchangeable here (but not when they are simple)
+-- -- complex or captured values (including simple ones) go in linear memory
+-- -- captured values are passed as additional reference arguments to the function
+-- 
+-- inc :: Int -> ST.State Int Int
+-- inc x = do
+--   prev <- ST.get
+--   ST.put prev
+--   pure (prev + x)
+-- 
+-- memLayoutE :: Expr -> ST.State Int (Maybe Frame)
+-- memLayoutE = undefined
+-- 
+-- memLayoutS :: [Statement] -> Expr -> ST.State Int Frame
+-- memLayoutS sts ret = undefined
+--   where
+--     returnBinding :: Maybe Ident
+--     returnBinding = case ret of
+--       Var v -> Just v
+--       _ -> Nothing
+-- 
+--     a :: ST.State Int (Map Ident BindingOffset)
+--     a = M.fromList . concat <$> sequence
+--       [ case st of
+--           SBinding n t _
+--             | Just n == returnBinding -> pure [(n, ReturnOffset)]
+--             | otherwise -> do
+--                 offset <- inc (typeSize t)
+--                 pure [(n, OffsetRelToFrame offset)]
+--           SFor _ _ sts' -> do
+--             frame <- memLayoutS sts' Unit
+--             _ <- inc frame.size
+--             pure []
+--       | st <- sts
+--       ]
+-- 
+-- -- inline :: Expr -> Expr -> ParentStatementsZipper -> ParentStatementsZipper
+-- -- inline (App (Abs TUnit rt ss r) Unit) = undefined
---
+++ src/OSC/Box.hs
@@ -0,0 +1,41 @@
+module OSC.Box where
+
+data Number = I Int | F Double
+  deriving Show
+
+data Ident = Ident String
+  deriving Show
+
+data Box
+  = BConst Number
+  | BVar Ident
+  | BDelay Ident Int Box
+  | BFunc String Box Box
+
+flow :: Box -> String
+flow (BConst n) = show n
+flow (BVar (Ident v)) = v
+flow (BFunc f a b) = "((" <> flow a <> ") " <> f <> " (" <> flow b <> "))"
+flow (BDelay (Ident n) _ _) = n
+
+gatherDelays :: Box -> [(Ident, Box)]
+gatherDelays (BDelay n _ b) = [(n, b)]
+gatherDelays (BFunc _ a b) = gatherDelays a <> gatherDelays b
+gatherDelays _ = []
+
+codegen :: Box -> IO ()
+codegen b = do
+  putStrLn $ "out = " <> flow b
+  sequence_
+    [ putStrLn $ n <> " = " <> flow b'
+    | (Ident n, b') <- gatherDelays b
+    ]
+
+b1 :: Box
+b1 = BFunc "+" (BConst (I 5)) (BVar (Ident "sample_rate"))
+
+b2 :: Box
+b2 = res
+  where
+    prev = BDelay (Ident "prev" ) 1 res
+    res = BFunc "+" prev (BConst (I 1))
