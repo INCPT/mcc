28d8f4266e7571377eacb9a73cdbb88d4af7c8b7
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-03-12 15:35:30 +0200

    refactor: reorganize Box definitions and simplify exprToBox

--- src/OSC/Box.hs
+++ src/OSC/Box.hs
@@ -52,6 +52,18 @@ b2 = res
     prev = BDelay (Ident "prev" ) 1 res
     res = BFunc "+" prev (BConst (I 1))
 
+-- desugar :: Map Ident Expr -> Graph -> State BoxIndex [LBox]
+-- desugar env (Graph bindings ret) = sequence
+--   [ undefined
+--   | Binding n e <- bindings
+--   ]
+--   where
+--     -- TODO no shadowing etc
+--     innerEnv =  M.fromList
+--       [ (n, e)
+--       | Binding n e <- bindings
+--       ]
+
 --------------------------------------------------------------------------------
 
 data Binding expr = Binding Ident expr
@@ -61,6 +73,8 @@ data Binding expr = Binding Ident expr
 -- TODO: normal functions/methods not in scope in graph (only variables are in scope)
 -- TODO: branch operation computes both branches
 -- TODO: after the shadow check/SSA pass all Idents are unique
+
+-- TODO: should this be legal: f: f32[4] -> f32, rec |prev| return (f prev)
 
 data Index a = IConst Int | IVar a
   deriving (Show, Functor)
@@ -105,15 +119,6 @@ data Graph = Graph [Binding Expr] Expr
 newtype BoxIndex = BoxIndex Int
   deriving (Num, Eq, Ord, Show)
 
-data LBox
-  = LBConst Number
-  | LBVar Ident
-  | LBArr [LBox]
-  | LBSelect LBox [Index LBox]
-  | LBDelay Int LBox
-  | LBCall Ident [LBox] -- TODO: func must be pure
-  deriving Show
-
 {-
 inlineExpr :: Map Ident Expr -> [Binding Expr] -> Expr -> Expr
 inlineExpr env bindings expr = inline (env `M.union` bindingMap) expr
@@ -145,6 +150,13 @@ drill _ boxes is = (boxes, is)
 --   | Just (LBArr boxes') <- M.lookup (boxes !! n) env = drill env boxes' ns
 -- drill _ boxes is = Left (boxes, is)
 
+flattenBox :: LBox -> LBox
+flattenBox (LBArr boxes) = case map flattenBox boxes of
+  [box] -> box
+  boxes -> LBArr boxes
+flattenBox box = box
+
+
 
 newBox :: LBox -> State (Map BoxIndex LBox) BoxIndex
 newBox box = do
@@ -152,6 +164,17 @@ newBox box = do
   let nextIdx = BoxIndex (M.size boxes)
   ST.put (M.insert nextIdx box boxes)
   return nextIdx
+
+--------------------------------------------------------------------------------
+
+data LBox
+  = LBConst Number
+  | LBVar Ident
+  | LBArr [LBox]
+  | LBSelect LBox [Index LBox] -- maximally drilled into
+  | LBDelay Int LBox
+  | LBCall Ident [LBox]
+  deriving Show
 
 data Env = Env
   { identToBox :: Map Ident LBox
@@ -167,70 +190,25 @@ exprToBox env (ERec delay n ret) = retBox
       (env { identToBox = M.insert n delayBox env.identToBox })
       ret
     delayBox = LBDelay delay retBox
-exprToBox env (EArr es) = LBArr
-  [ flattenBox (exprToBox env expr)
-  | expr <- es
-  ]
+exprToBox env (EArr es) = LBArr (map (exprToBox env) es)
 exprToBox env (ESelect e is) = LBSelect
   (exprToBox env e)
   (map (fmap (exprToBox env)) is)
-
-exprToBox env (ECall n args) = LBCall n (map (flattenBox . exprToBox env) args)
-
-flattenBox :: LBox -> LBox
-flattenBox (LBArr boxes) = case map flattenBox boxes of
-  [box] -> box
-  boxes -> LBArr boxes
-flattenBox box = box
-
--- TODO: should this be legal: f: f32[4] -> f32, rec |prev| return (f prev)
-
---------------------------------------------------------------------------------
--- Example to verify that RecursiveDo with lazy State doesn't diverge
--- This mirrors the pattern used in exprToBoxes for ERec
-exampleRecDo :: State (Map Int String) [Int]
-exampleRecDo = do
-  rec
-    -- Use the result of computation that depends on 'keys'
-    result <- traverse (\k -> ST.modify (M.insert k ("value" ++ show k)) >> return k) keys
-    -- Define 'keys' based on something that uses 'result' indirectly
-    -- but the actual list structure [1,2,3] is available immediately (lazy)
-    let keys = [1, 2, 3]
-  return result
-
--- More direct analog to the ERec pattern:
--- Creating boxes that reference each other through delays
--- exampleDelayPattern :: State (Map BoxIndex LBox) [BoxIndex]
--- exampleDelayPattern = do
---   rec
---     -- Create return boxes that reference the delay boxes
---     retBoxes <- traverse newBox [LBCall "+" (boxes !! 0) (BoxIndex 100), LBCall "*" (boxes !! 1) (BoxIndex 200)]
---     -- Create delay boxes that reference the return boxes
---     boxes <- traverse (\retBox -> newBox (LBDelay 1 retBox)) retBoxes
---   return retBoxes
-
--- Test function to run the example
-testRecDo :: IO ()
-testRecDo = do
-  putStrLn "Testing RecursiveDo with lazy State:"
-  let (result, finalState) = ST.runState exampleRecDo M.empty
-  putStrLn $ "Result: " ++ show result
-  putStrLn $ "Final state: " ++ show finalState
-  putStrLn "\nThis demonstrates that the pattern won't diverge because:"
-  putStrLn "1. The list structure [1,2,3] is available immediately (spine is strict)"
-  putStrLn "2. The State monad is lazy, so we can reference future computations"
-  putStrLn "3. The recursion is productive - each step produces observable output"
+exprToBox env (ECall n args) = LBCall n (map (exprToBox env) args)
 
 --------------------------------------------------------------------------------
 
--- desugar :: Map Ident Expr -> Graph -> State BoxIndex [LBox]
--- desugar env (Graph bindings ret) = sequence
---   [ undefined
---   | Binding n e <- bindings
---   ]
---   where
---     -- TODO no shadowing etc
---     innerEnv =  M.fromList
---       [ (n, e)
---       | Binding n e <- bindings
---       ]
+-- TODO: after component clustering, if a component is called only once, inline
+
+data Addr
+
+data Local = Simple Addr | Array Addr Int
+
+data Block
+  = Local (Local -> Block)
+  | Load Local Ident
+  | Write Local Block -- local, value
+  | WriteArr Local Block Block -- local, index, value
+  | Call Ident [Local]
+  
+data Program = Program [Block] Local -- execute block, return local
