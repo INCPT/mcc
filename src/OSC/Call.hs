8a2185dce9e615eab4c22ba7449c67f23fd3b625
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-03-23 06:40:02 +0200

    refactor: simplify lower function and fix Identity import

--- src/OSC/Call.hs
+++ src/OSC/Call.hs
@@ -10,7 +10,7 @@
 
 module OSC.Call where
 
-import Data.Functor.Identity (Identity)
+import Data.Functor.Identity (Identity (Identity))
 import Control.Monad (when)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.Trans (MonadTrans, lift)
@@ -108,32 +108,15 @@ data Expr
 funcref :: IR (ST.State Mut) Ref -> IR (ST.State Mut) Ref
 funcref = undefined
 
-lower :: IR (ST.State Mut) () -> (Mut, IR Identity ())
-lower ir = ST.runState (lowerFreeT ir) (Mut M.empty 0)
-  where
-    lowerFreeT :: FreeT (ST.State Mut) IRF () -> ST.State Mut (IR Identity ())
-    lowerFreeT (FreeT m) = do
-      step <- m
-      case step of
-        Pure a -> return (return a)
-        Free (Alloc t b k) -> do
-          rest <- lowerFreeT (k undefined)
-          return $ FreeT $ Identity $ Free $ Alloc t b (\r -> rest)
-        Free (Arg i t k) -> do
-          rest <- lowerFreeT (k undefined)
-          return $ FreeT $ Identity $ Free $ Arg i t (\r -> rest)
-        Free (CopyVal n r l next) -> do
-          rest <- lowerFreeT next
-          return $ FreeT $ Identity $ Free $ CopyVal n r l rest
-        Free (CopyRef r1 r2 l next) -> do
-          rest <- lowerFreeT next
-          return $ FreeT $ Identity $ Free $ CopyRef r1 r2 l rest
-        Free (BinOp op r1 r2 r3 next) -> do
-          rest <- lowerFreeT next
-          return $ FreeT $ Identity $ Free $ BinOp op r1 r2 r3 rest
-        Free (Call r rs r' next) -> do
-          rest <- lowerFreeT next
-          return $ FreeT $ Identity $ Free $ Call r rs r' next
+lower :: IR (ST.State Mut) () -> ST.State Mut (IR Identity ())
+lower (FreeT (Alloc t b next)) = undefined
+
+--   case (next undefined) of
+--     Pure a -> pure ()
+--     Free f -> do
+--       r <- ST.StateT (ST.runStateT f)
+--       s <- runFreeT (_ r)
+--       undefined
 
 -- bla :: Expr -> ST.StateT Mut (R.Reader Env) IR
 -- bla = undefined
