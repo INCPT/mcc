4ae83a0738ea148db20dc70a6e276ac755fae01c
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-03-25 18:19:42 +0200

    refactor: simplify IR types and remove unused definitions

--- src/OSC/Call.hs
+++ src/OSC/Call.hs
@@ -22,29 +22,8 @@ import qualified Data.Map as M
 import Control.Monad.Free (Free (Free, Pure), liftF)
 import qualified Control.Monad.Trans.Free as TF
 import Control.Monad.Trans.Free (FreeT (FreeT), FreeF)
+import OSC.Ctx
 
-data Type = TNumber | TArr Type {- length -} Int | TAbs Type Type
-  deriving Show
-
-sizeOfType :: Type -> Int
-sizeOfType = undefined
-
-paramTypes :: Type -> [Type]
-paramTypes (TAbs t r) = t:paramTypes r
-paramTypes _ = []
-
-data VType = VTNumber | VTArr Type Int
-
-returnType :: Type -> VType
-returnType TNumber = VTNumber
-returnType (TArr t dim) = VTArr t dim
-returnType (TAbs _ r) = returnType r
-
-data Ident = Ident String deriving (Eq, Ord, Show)
-data Number = I32 Int | I64 Int | F32 Float | F64 Double deriving (Eq, Ord, Show)
-
-newtype FuncRef = FuncRef Int deriving (Eq, Ord, Show)
-newtype GlobalIdx = GlobalIdx Int deriving (Eq, Ord, Show)
 data Idx = Local Int | Global Int deriving (Eq, Ord, Show)
 newtype ArrayBaseAddr = ArrayBaseAddr Idx deriving (Eq, Ord, Show)
 newtype ArgPos = ArgPos Int deriving (Eq, Ord, Show)
@@ -58,14 +37,6 @@ data Ref
   | RRArray Type Idx -- contains the local/global var index containing the base address of an array
   | RRFuncRef Idx -- contains the local/global var index containing the index into the function table
   deriving Show
-
-data Value = VConst Int | VVar Idx | VVVar Idx
-  deriving Show
-
-refToValue :: Either Int Ref -> Value
-refToValue (Left idx) = VConst idx
-refToValue (Right (RVar idx)) = VVar idx
-refToValue e = error $ "refToValue: " <> show e <> " (this is a bug)"
 
 --------------------------------------------------------------------------------
 
@@ -87,23 +58,16 @@ refToValue e = error $ "refToValue: " <> show e <> " (this is a bug)"
 
 -- NOTE: a literal array paired with a selection is a choice
 
-data Lens = Lens { from :: [Value], to :: [Int] }
+newtype Lens = Lens { to :: [Int] }
 
-focusLensFrom :: Value -> Lens -> Lens
-focusLensFrom i l = l { from = i:l.from }
-
-focusLensTo :: Int -> Lens -> Lens
-focusLensTo i l = l { to = i:l.to }
+focusLens :: Int -> Lens -> Lens
+focusLens i l = l { to = i:l.to }
 
 data Env = Env {
   typ :: Type,
   ret :: Ref,
   lens :: Lens
 }
-
-data Op
-
-data AllocRegion = ARGlobal | ARLocal
 
 data IRF n
   = Ref Ref
@@ -112,146 +76,32 @@ data IRF n
   | Alloc Type AllocRegion (Ref -> n)
 
   -- copies a constant into the Ref that must be an RVar or an RArray/RRArray with lens.to focused on a single element
-  | CopyConst Type Number Ref Lens n
+  | CopyConst Type Number Ref Lens
 
   -- copies the value referenced by the first Ref into the second Ref respecting both lens.from and lens.to
   -- the N-D (N dimensional) lens describes two N-D subslices of an M-D source tensor to a a K-D destination tensor (N >= 1, M >= N, K >= N)
-  | CopyRef Type Ref Ref Lens n
+  | CopyRef Type Ref Ref Lens
 
-  | BinOp Op Ref Ref Ref n
+  | BinOp Op Ref Ref Ref
 
   | Call Ref [Ref] Ref -- first Ref must be an RFuncRef or an RRFuncRef; then arguments; then destination
-  deriving (Functor)
+
+  | Done
+  deriving Functor
 
 type IR = Free IRF
-type IRT = FreeT IRF
 
-alloc :: Type -> AllocRegion -> IR Ref
-alloc t b = liftF (Alloc t b id)
-
-arg :: Int -> IR ()
-arg i = liftF (Arg i)
-
-call :: Ref -> [Ref] -> Ref -> IR ()
-call fr args ret = Free (Call fr args ret)
-
--- copyVal :: Number -> Ref -> Lens -> IR m ()
--- copyVal n r l = liftF (CopyVal n r l ())
--- 
--- copyRef :: Ref -> Ref -> Lens -> IR m ()
--- copyRef r1 r2 l = liftF (CopyRef r1 r2 l ())
--- 
--- binOp :: Op -> Ref -> Ref -> Ref -> IR m ()
--- binOp op r1 r2 r3 = liftF (BinOp op r1 r2 r3 ())
--- 
--- call :: Ref -> [Ref] -> Ref -> IR m ()
--- call r rs r' = liftF (Call r rs r' ())
-
-data Mut = Mut {
-  funcRefs :: Map FuncRef (IR ()),
-  nextFuncRefIdx :: Int,
-  nextAlloc :: Int
-}
-
-data Expr
-
--- TODO: interpret allocates given an allocation strategy (Type Bool -> ST.State st Ref)
-
-{-
-interpret :: IR () -> ST.State Mut (IR ())
-interpret (Pure a) = pure $ Pure a
-interpret (Free (Abs t body)) = do
-  idx <- ST.gets (.nextFuncRefIdx); ST.modify $ \st -> st { nextFuncRefIdx = st.nextFuncRefIdx + 1 }
-  lbody <- interpret body
-  ST.modify $ \st -> st { funcRefs = M.insert (FuncRef idx) lbody st.funcRefs }
-  pure $ liftF $ Ref $ RFuncRef $ FuncRef idx
-interpret (Free (Alloc t g next)) = do
-  idx <- ST.gets (.nextAlloc); ST.modify $ \st -> st { nextAlloc = st.nextAlloc + 1 }
-  interpret (next $ RVar $ Local $ idx)
-interpret (Free (Ref r)) = pure $ liftF $ Ref r
-interpret (Free (Arg i)) = pure $ liftF $ Arg i
-interpret (Free (CopyVal t n r l next)) = do
-  rest <- interpret next
-  pure $ Free $ CopyVal t n r l rest
-interpret (Free (CopyRef t r1 r2 l next)) = do
-  rest <- interpret next
-  pure $ Free $ CopyRef t r1 r2 l rest
-interpret (Free (BinOp op r1 r2 r3 next)) = do
-  rest <- interpret next
-  pure $ Free $ BinOp op r1 r2 r3 rest
-interpret (Free (Call r rs r')) = pure $ Free $ Call r rs r'
-
-lower :: Env -> IRT (R.Reader Env) a -> IR a
-lower env m = case R.runReader (TF.runFreeT m) env of
-  TF.Pure a -> Pure a
-  TF.Free ir -> case ir of
-    Ref r -> Free $ Ref r
-    Arg i -> Free $ Arg i
-    Alloc t g next -> Free $ Alloc t g (\r -> lower env (next r))
-    CopyVal t n r l next -> Free $ CopyVal t n r l (lower env next)
-    CopyRef t r1 r2 l next -> Free $ CopyRef t r1 r2 l (lower env next)
-    BinOp op r1 r2 r3 next -> Free $ BinOp op r1 r2 r3 (lower env next)
-    Call r rs r' -> Free $ Call r rs r'
-    Abs t body -> Free $ Abs t (lower env body)
+copyConst :: Type -> Number -> Ref -> Lens -> IR ()
+copyConst t n ref lens = liftF $ CopyConst t n ref lens
 
 --------------------------------------------------------------------------------
 
-retVal :: Number -> IRT (R.Reader Env) ()
-retVal n = FreeT $ do
-  env <- R.ask
-  pure $ TF.Free $ CopyVal env.typ n env.ret env.lens (pure ())
-
-retRef :: Ref -> IRT (R.Reader Env) ()
-retRef ref = FreeT $ do
-  env <- R.ask
-  pure $ TF.Free $ CopyRef env.typ ref env.ret env.lens (pure ())
-
--- API -------------------------------------------------------------------------
-
-select :: IRT (R.Reader Env) a -> Either Int Ref -> IRT (R.Reader Env) a
-select ir ref = TF.hoistFreeT (R.local $ \env -> env { lens = focusLensFrom (refToValue ref) env.lens }) ir
-
-focus :: Int -> IRT (R.Reader Env) a -> IRT (R.Reader Env) a
-focus i = TF.hoistFreeT $ R.local $ \env -> env { lens = focusLensTo i env.lens }
-
-choice :: [IRT (R.Reader Env) ()] -> Ref -> IRT (R.Reader Env) ()
-choice = undefined
-
-allocAndCall :: Type -> AllocRegion -> IRT (R.Reader Env) () -> IRT (R.Reader Env) Ref
-allocAndCall t region ir = TF.FreeT $ pure $ TF.Free $ Alloc t region $ \ref -> TF.FreeT $ R.local (const $ env ref) $ TF.runFreeT (ir >> pure ref)
-  where
-    env ref = Env {
-      typ = t,
-      ret = ref,
-      lens = Lens { from = [], to = [] }
-    }
-
-funcRef :: IRT (R.Reader Env) (Either Number Ref) -> IRT (R.Reader Env) ()
-funcRef ir = TF.FreeT $ TF.runFreeT (ir >>= either retVal retRef)
-
---------------------------------------------------------------------------------
-
-newtype CallM m a = CallM { callM :: R.ReaderT Env (ST.StateT Mut m) a }
-  deriving (Functor, Applicative, Monad) -- , MonadTrans, MFunctor)
-
--- this doesn't need to be here
--- withBindings :: MonadFix m => [(Ident, CallM m Ref)] -> CallM m () -> CallM m ()
--- withBindings bs f = CallM $ mdo
---   bsRefs <- fmap M.fromList $ sequence
---     [ do
---         r <- R.local (\env -> env { refs = bsRefs <> env.refs }) b.callM
---         pure (i, r)
---     | (i, b) <- bs
---     ]
+-- choiceToIR :: Choice -> R.Reader Env IR
+-- choiceToIR (CExpr _ (SConst n)) = do
+--   env <- R.ask
+--   pure $ CopyConst TNumber n env.ret env.lens Done
+-- choiceToIR (CExpr [] (SArr t elems)) = do
+--   undefined
+-- choiceToIR (CExpr _ (SArr _ _)) = error "choiceToIR: SArr: non empty selection indices (this is a bug)"
 -- 
---   R.local (\env -> env { refs = bsRefs <> env.refs }) f.callM
--- 
--- -- this doesn't need to be here
--- capture :: Monad m => Ident -> CallM m Ref
--- capture n = CallM $ R.asks (M.lookup n . (.refs)) >>= \case
---   Just ref -> pure ref
---   Nothing -> error "capture: no binding (this is a bug)"
--- 
--- external :: Ident -> [Ref] -> CallM m ()
--- external = undefined
--}
+-- choiceToIR _ = undefined
