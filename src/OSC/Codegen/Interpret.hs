edd84bfae41cc10dca55ef5e856163aa5d0b40bf
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-04-12 16:32:22 +0300

    feat: add Value type and interpretToList function stub

--- src/OSC/Codegen/Interpret.hs
+++ src/OSC/Codegen/Interpret.hs
@@ -1,3 +1,10 @@
 module OSC.Codegen.Interpret where
 
+import OSC.Codegen
 import OSC.Codegen.Backend
+
+data Value = VNumber Number | VArr [Value]
+  deriving Show
+
+interpretToList :: Int -> IR -> [Ref]
+interpretToList steps = undefined
