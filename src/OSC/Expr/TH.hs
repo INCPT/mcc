ed41be6fca51bd45e5c9e06d0ae86738b0b84838
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-04-15 01:49:38 +0300

    refactor: replace foldM with foldr in makeTransformBody

--- src/OSC/Expr/TH.hs
+++ src/OSC/Expr/TH.hs
@@ -37,85 +37,6 @@ class BiPlate a b c | a c -> b, b c -> a where
 
 --------------------------------------------------------------------------------
 
-{-
--- Simple recursive functor (can be paired with Identity)
-data Mu f = Mu (f (Mu f))
-
--- Annotated recursive functor + monad
-data Ann ann f = Ann (ann, f (Ann ann f))
-data AnnM ann expr a = AnnM (Ann ann expr -> a)
-
--- DAG recursive functor + monad
-data Dag k f = Node (f (Dag k f)) | Key k
-data DagM k expr a = DagM ((k -> expr (Dag k expr)) -> a)
--}
-
--- write a TH function that:
-
----- having the following types
-
-data Value exp = Const Int | Arr [exp]
-data Expr exp = Add exp exp | Mul exp exp
-
----- and calling
-
------- makeSum "S_" "Sum0" [''Value, ''Expr]
-
----- will generate the following datatype:
-
-data Sum0 exp
-  = S_Const Int
-  | S_Arr [exp]
-  | S_Add exp exp
-  | S_Mul exp exp
-  deriving (Functor, Foldable, Traversable)
-
----- and implement the Plate and Biplate classes:
-
-instance Plate Sum0 where
-  descend unwrap extract expr = do
-    inner <- unwrap expr
-    a <- extract inner
-    case a of
-      Just a' -> pure [a']
-      Nothing -> case inner of
-        S_Const _ -> pure []
-        -- if field is of type `h (g (f exp)) ...` (like `[exp]` or `[Maybe exp]`) then just traverse (and figure out how to do nested traversls) - we'll throw a type error if f isn't a Traversable
-        S_Arr exprs -> fmap mconcat $ traverse (descend unwrap extract) exprs
-        S_Add exp1 exp2 -> (<>) <$> descend unwrap extract exp1 <*> descend unwrap extract exp2
-        S_Mul exp1 exp2 -> (<>) <$> descend unwrap extract exp1 <*> descend unwrap extract exp2
-
-instance BiPlate Sum0 Sum0 Sum0 where
-  transformBi unwrap wrap f expr = do
-    inner <- unwrap expr
-    case inner of
-      S_Const n   -> wrap =<< pure (S_Const n)
-      S_Arr exprs -> wrap =<< f =<< (S_Arr <$> traverse (transformBi unwrap wrap f) exprs)
-      S_Add a b   -> wrap =<< f =<< (S_Add <$> transformBi unwrap wrap f a <*> transformBi unwrap wrap f b)
-      S_Mul a b   -> wrap =<< f =<< (S_Add <$> transformBi unwrap wrap f a <*> transformBi unwrap wrap f b)
-  
----- calling
-
------- makeDiff "D_" "Diff_Sum0_Expr" ''Sum0 ''Value
-
----- will generate the following datatype:
-
-data Diff_Sum0_Expr exp
-  = D_Add exp exp
-  | D_Mul exp exp
-
----- and implement the following BiPlate class:
-
-instance BiPlate Sum0 Value Diff_Sum0_Expr where
-  transformBi unwrap wrap f expr = do
-    inner <- unwrap expr
-    case inner of
-      S_Const n -> wrap =<< pure (Const n)
-      S_Arr as  -> wrap =<< (Arr <$> traverse (transformBi unwrap wrap f) as)
-
-      S_Add a b -> wrap =<< f =<< (D_Add <$> transformBi unwrap wrap f a <*> transformBi unwrap wrap f b)
-      S_Mul a b -> wrap =<< f =<< (D_Add <$> transformBi unwrap wrap f a <*> transformBi unwrap wrap f b)
-
 --------------------------------------------------------------------------------
 
 makeSum :: String -> String -> [Name] -> Q [Dec]
@@ -343,7 +264,7 @@ makeTransformBody targetConName fields fieldVars unwrapVar wrapVar fVar mode = d
           _ -> 
             [| transformBi $(varE unwrapVar) $(varE wrapVar) $(varE fVar) $(varE var) |]
 
-      conApp <- foldM (\acc field -> appE (pure acc) field) (conE targetConName) transformedFields
+      conApp <- foldr (\acc field -> appE (pure acc) field) (conE targetConName) (transformedFields)
       
       case mode of
         ApplyF -> [| $(varE wrapVar) =<< $(varE fVar) =<< $(pure conApp) |]
