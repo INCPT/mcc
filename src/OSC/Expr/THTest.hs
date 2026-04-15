8b196d7148db2824b3c1d073b52e34a6f598c198
Philip Kamenarsky <p.kamenarsky@gmail.com>
2026-04-15 06:24:43 +0300

    POC

--- src/OSC/Expr/THTest.hs
+++ src/OSC/Expr/THTest.hs
@@ -10,6 +10,7 @@
 
 module OSC.Expr.THTest where
 
+import Data.Functor.Identity
 import qualified Data.Foldable as F
 
 import OSC.Expr.TH
@@ -27,55 +28,63 @@ data Dag k f = Node (f (Dag k f)) | Key k
 data DagM k expr a = DagM ((k -> expr (Dag k expr)) -> a)
 -}
 
-data Mu f = Mu (f (Mu f))
+data Mu f = Mu { unmu :: f (Mu f) }
 
 -- write a TH function that:
 
 ---- having the following types
 
 data Value exp = Const Int | Arr [exp]
-data Expr exp = Single | Add (Maybe exp) exp | Mul (Maybe (Either String [exp])) exp | Exp (Maybe (Maybe (Maybe exp)))
+data Expr exp = Single | Add (Maybe exp) exp | Mul (Maybe (Either String [exp])) exp | Exp (Maybe (Maybe (Maybe exp))) (Maybe exp) (Maybe (Maybe exp))
 data Lambda exp = Lambda String [(String, exp)] exp
+data FuncRef exp = FuncRef Int
 
-$(makeSum "S1_" "Sum1" [''Value, ''Expr])
+$(makeSum "S1_" "Sum1" [''Value, ''Expr, ''FuncRef])
 $(makeSum "S2_" "Sum2" [''Value, ''Expr, ''Lambda])
 
--- $(makePlateInstance ''Sum1)
--- $(makePlateInstance ''Sum2)
+$(makePlateInstance ''Sum1)
+$(makePlateInstance ''Sum2)
 
 $(makeDiff "D1_" "Diff1" ''Sum1 ''Value)
 $(makeDiff "D2_" "Diff2" ''Sum2 ''Sum1)
 
-instance BiPlate Sum1 Value Diff1 where
-  transformBi unwrap wrap f expr = do
-    inner <- unwrap expr
-    case inner of
-      S1_Const n -> wrap =<< (Const <$> pure n)
-      S1_Arr as  -> wrap =<< (Arr <$> traverse (transformBi unwrap wrap f) as)
+-- $(makeBiPlateInstance ''Sum1 ''Value ''Diff1)
+$(makeBiPlateInstance ''Sum2 ''Sum1 ''Diff2)
 
-      S1_Add a b -> wrap =<< f =<< (D1_S1_Add <$> (traverse (transformBi unwrap wrap f)) a <*> transformBi unwrap wrap f b)
-      S1_Mul a b -> wrap =<< f =<< (D1_S1_Mul <$> (traverse (traverse (traverse (transformBi unwrap wrap f)))) a <*> transformBi unwrap wrap f b)
-      _ -> undefined
+sum2 :: Mu Sum2
+sum2 = undefined
 
-instance Plate Sum1 where
-  descend unwrap extract expr = do
-    inner <- unwrap expr
-    a <- extract inner
-    case a of
-      Just a' -> pure [a']
-      Nothing -> case inner of
-        S1_Const _ -> pure []
-        S1_Arr exprs -> (foldMapM (descend unwrap extract)) (F.toList exprs)
-        S1_Add exp1 exp2 -> (<>) <$> (foldMapM (descend unwrap extract)) (F.toList exp1) <*> descend unwrap extract exp2
-        S1_Mul exp1 exp2 -> (<>) <$> (foldMapM (descend unwrap extract)) (foldList $ F.toList $ foldList $ F.toList exp1) <*> descend unwrap extract exp2
-        S1_Exp exp1 -> (foldMapM (descend unwrap extract)) (foldList $ F.toList $ foldList $ F.toList exp1)
-        _ -> undefined
+test :: Identity (Mu Sum1)
+test = transformBi (pure . unmu) (pure . Mu) go sum2
+  where
+    go :: Diff2 (Mu Sum1) -> Identity (Sum1 (Mu Sum1))
+    go (D2_S2_Lambda n bindings body) = pure $ S1_FuncRef 5
 
-foldMapM :: Applicative f => Monoid b => (a -> f b) -> [a] -> f b
-foldMapM f = fmap mconcat . traverse f
+-- instance BiPlate Sum1 Value Diff1 where
+--   transformBi unwrap wrap f expr = do
+--     inner <- unwrap expr
+--     case inner of
+--       S1_Const n -> wrap =<< (Const <$> pure n)
+--       S1_Single -> wrap =<< f =<< pure D1_S1_Single
+--       S1_Arr as  -> wrap =<< (Arr <$> traverse (transformBi unwrap wrap f) as)
+-- 
+--       S1_Add a b -> wrap =<< f =<< (D1_S1_Add <$> (traverse (transformBi unwrap wrap f)) a <*> transformBi unwrap wrap f b)
+--       S1_Mul a b -> wrap =<< f =<< (D1_S1_Mul <$> (traverse (traverse (traverse (transformBi unwrap wrap f)))) a <*> transformBi unwrap wrap f b)
+--       _ -> undefined
 
-foldList :: Foldable t => Applicative t => [t a] -> [a]
-foldList = mconcat . F.toList . sequenceA
+-- instance Plate Sum1 where
+--   descend unwrap extract expr = do
+--     inner <- unwrap expr
+--     a <- extract inner
+--     case a of
+--       Just a' -> pure [a']
+--       Nothing -> case inner of
+--         S1_Const _ -> pure []
+--         S1_Arr exprs -> (foldMapM (descend unwrap extract)) (F.toList exprs)
+--         S1_Add exp1 exp2 -> (<>) <$> (foldMapM (descend unwrap extract)) (F.toList exp1) <*> descend unwrap extract exp2
+--         S1_Mul exp1 exp2 -> (<>) <$> (foldMapM (descend unwrap extract)) (foldList $ F.toList $ foldList $ F.toList exp1) <*> descend unwrap extract exp2
+--         S1_Exp exp1 -> (foldMapM (descend unwrap extract)) (foldList $ F.toList $ foldList $ F.toList exp1)
+--         _ -> undefined
 
 -- $(do
 --   decs <- makePlateInstance ''Sum1
